768fb7d474f99cc156a082d446c2d9f586d8da05
Runo22 <tunonur22@gmail.com>
2021-05-10 18:39:32 +0300

    otonom

--- lib/pages/control.dart
+++ lib/pages/control.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:control_pad/control_pad.dart';
-import 'package:esp_control/pages/otonom.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
