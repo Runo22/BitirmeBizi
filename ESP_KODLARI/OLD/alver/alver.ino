10621bcff919a9de49aa0350d60ff8ffa42b64ca
Runo22 <tunonur22@gmail.com>
2021-05-29 16:09:28 +0300

    otonom ve yavasla kodu eklendi

--- ESP_KODLARI/OLD/alver/alver.ino
+++ ESP_KODLARI/OLD/alver/alver.ino
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=gZhUi24_qms&ab_channel=ACROBOTIC
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -12,19 +13,18 @@ byte ID[4] = {6,16,239,197};
 
 const char *ssid = "SUPERONLINE_WiFi_4614";
 const char *password = "t5CuNKueHphC";
-IPAddress ip(192, 97, 1, 200);
+IPAddress ip(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 ESP8266WebServer server(80);
 
 String  resp;
-bool isYavas = false;
 
 
-//void handleRoot()   //TODO DEL:GEREKSİZ SANIRIM
-//{
-//    server.send(200, "text/html", "You are connected");
-//}
+void handleRoot()
+{
+    server.send(200, "text/html", "You are connected");
+}
 
 void setup() {
   Serial.begin(9600);
@@ -46,15 +46,10 @@ void setup() {
 }
 
 void handle(){
-  if(isYavas == true){
-    server.send(200,"text/plain","yavas");
-    isYavas == false;
-  }else{
-    server.send(200,"text/plain","CONNECTED"); 
-  }
-  resp = server.arg("value");
-  //veri = resp.substring(0,2).toInt();
-  Serial.print(resp + "*");                   // * = 42 ascii
+  server.send(200,"text/plain","CONNECTED");
+    resp = server.arg("value");
+    //veri = resp.substring(0,2).toInt();
+    Serial.print(resp);                   // * = 42 ascii
 }
 
 void handleOtonom(){
@@ -64,9 +59,6 @@ void handleOtonom(){
     if (otonomDurum == 77){
       //otonom iş bitince bu alttaki gidicek
       server.send(200,"text/plain","ELLE");
-    }
-    else{
-      server.send(200,"text/plain","CONNECTED");
     }
   }
   delay(1);   //??????
@@ -90,16 +82,22 @@ void readKey(){
    rfid.uid.uidByte[1] == ID[1] &&
    rfid.uid.uidByte[2] == ID[2] &&
    rfid.uid.uidByte[3] == ID[3]){
-    //okuduğu zaman listeden random durum seçip uygulmaaya gönderilecek.
-//    Serial.println("YAVAŞ!!!");
-    //burdan uygulamay veri gönderilecek 
-    //uygulmadan t sniye boyunca hız 1 olacak
-    //ve yavaşla işareti çıkacak
-    isYavas = true;
+    Serial.println("YAVAŞ!!!");
+    printKey();
+    delay(1000);
     }
-//   else{
-//    Serial.print("Yetkisiz KART!!!");
-//    }   
+   else{
+    Serial.print("Yetkisiz KART!!!");
+    printKey();
+    }   
    rfid.PICC_HaltA();
   }
   
+void printKey(){
+  Serial.print("ID numarası: ");
+  for(int i = 0; i < 4; i++){
+    Serial.print(rfid.uid.uidByte[i]);
+    Serial.print(" ");
+    }
+   Serial.println(" ");
+  }
